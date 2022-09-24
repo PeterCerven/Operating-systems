4fb808de4673503e1a8cc44cefb86925df7db933
Peter Cerven <peter.cerven33@gmail.com>
2022-09-24 16:03:05 +0200

    feat: implement sleep

---
+++ user/sleep.c
@@ -0,0 +1,20 @@
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char* argv[])
+{
+  int ticks;
+  
+  if(argc != 2){
+    fprintf(2, "wrong number of arguments\n");	  
+  }
+
+  ticks = atoi(argv[1]);
+
+  sleep(ticks);
+
+  exit(0);
+}
